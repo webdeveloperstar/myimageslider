b8f6f55b458129b8b79ba10bdf5df8468c2cffed
Gulam Rasool Khan <145851474+webdeveloperstar@users.noreply.github.com>
2024-04-27 18:23:36 +0530

    Add files via upload
    add....

---
+++ srcipt.js
@@ -0,0 +1,31 @@
+
+
+
+var tl = gsap.timeline({
+    repeat: -1
+});
+
+
+tl
+.to(".imagecontainer",{
+    ease: Expo.easeInOut,
+    width: "100%",
+    stagger: 2
+}, 'a')
+
+
+.to(".text h1",{
+    ease: Expo.easeInOut,
+    
+    stagger: 2,
+    top: 0
+}, 'a')
+
+
+.to(".text h1",{
+    ease: Expo.easeInOut,
+    delay: 2,
+    stagger: 2,
+    top: "-100%"
+}, 'a')
+
